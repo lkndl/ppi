1d7eab1fecbca4e0663bc7b3ce478ca7c7212cb3
Leo Kaindl <leo.kaindl@tum.de>
2023-03-14 04:56:34 +0100

    new huintaf dataset generation

--- colabfold/huintaf_seth.sh
+++ colabfold/huintaf_seth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 source /mnt/lsf-nas-1/os-shared/anaconda3/etc/profile.d/conda.sh
-conda activate kaindl_e3
+conda activate kaindl_ppi
 
-seth -i uniprot/huintaf.hash.fasta -o huintaf_seth_scores.json -f json -c /mnt/project/kaindl/ppi/embed_data/t5_xl_weights
+seth -i uniprot/huintaf2_fixed.fasta -o huintaf2_seth_scores.json -f json -c /mnt/project/kaindl/ppi/embed_data/t5_xl_weights
 
